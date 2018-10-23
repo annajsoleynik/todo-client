4af01c1f453aa69bd04b0b3c250dd01c8abde22a
annaoleynik <prigmaster@gmail.com>
2018-10-23 12:48:20 -0700

    handleOnInputName,handleOnInputDescription,

--- src/modules/todo/view/addTodoView.jsx
+++ src/modules/todo/view/addTodoView.jsx
@@ -4,30 +4,59 @@ import { Button, Col, Form, Row, FormGroup, Label, Input} from 'reactstrap';
 import _ from 'lodash';
 
 class AddTodoView extends Component {
-     state = [
+     state = {
          name: '',
          description: ''
-     ];
+     };
 
     render() {
         return (
             <div>
                 <h1>Add Todo</h1>
-                <Form>
+                <Form onSubmit={this.handleSubmit}>
                     <FormGroup>
                         <Label for="todoName">Name</Label>
-                        <Input type="text" name="name" id="todoName" placeholder="enter item's name" />
+                        <Input
+                            type="text"
+                            name="name"
+                            id="todoName"
+                            placeholder="enter item's name"
+                            value={this.state.name}
+                            onChange={this.handleOnInputName}
+                        />
                     </FormGroup>
                     <FormGroup>
                         <Label for="todoDescription">Description</Label>
-                        <Input type="textarea" name="description" id="todoDescription" />
+                        <Input
+                            type="textarea"
+                            name="description"
+                            id="todoDescription"
+                            value={this.state.description}
+                            onChange={this.handleOnInputDescription}
+                        />
                     </FormGroup>
                     <Button>Add Todo item</Button>
                 </Form>
             </div>
         );
     }
+
+    handleOnInputName = (event) => {
+        this.setState({name: event.target.value});
+
+    }
+
+    handleOnInputDescription = (event) => {
+        this.setState({description: event.target.value});
+
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
 }
+
+
 
 const mapStateToProps = state => ({
     // todoCurrentInfo: state.todo.todoCurrentInfo,
